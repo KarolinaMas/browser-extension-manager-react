6a0005cdb3096a9ddf78b0ebb4f8f818a8f54d76
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-06-26 10:34:26 +0300

    style list items

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,20 @@ const App = () => {
         </button>
       </header>
       <main>
-        <section className="my-10 text-center">
+        <section className="my-10 block place-items-center">
           <h1 className="text-[34px] font-bold leading-[-1px] text-[#091540] mb-6">
             Extensions List
           </h1>
-          <ul>
-            <li>All</li>
-            <li>Active</li>
-            <li>Inactive</li>
+          <ul className="flex gap-3">
+            <li className="bg-[#FBFDFE] px-5 pt-2 pb-2.5 text-[#091540] text-[20px] rounded-full border-1 border-[#D6E2F5] shadow-[0px_1px_2px_0px_rgba(217,229,244,0.75)] hover:bg-red-700 hover:text-[#FBFDFE]">
+              All
+            </li>
+            <li className="bg-[#FBFDFE] px-5 pt-2 pb-2.5 text-[#091540] text-[20px] rounded-full border-1 border-[#D6E2F5] shadow-[0px_1px_2px_0px_rgba(217,229,244,0.75)] hover:bg-red-700 hover:text-[#FBFDFE]">
+              Active
+            </li>
+            <li className="bg-[#FBFDFE] px-5 pt-2 pb-2.5 text-[#091540] text-[20px] rounded-full border-1 border-[#D6E2F5] shadow-[0px_1px_2px_0px_rgba(217,229,244,0.75)] hover:bg-red-700 hover:text-[#FBFDFE]">
+              Inactive
+            </li>
           </ul>
         </section>
       </main>
